3bdfb682d91b5459661950f0a94ff0316719219a
Farhan Anshari Miraj <farhananshari77@gmail.com>
2021-03-03 16:47:34 +0700

    route

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -3,33 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Guest;
 
 class GuestController extends Controller{
     //
-    public function createform(request $_REQUEST) {
-        return view('guest');  
-}
-public function GuestController(request $request) {
-
-    $this->validate($request,[
-        'nama_tamu' => 'required',
-        'tlp' => 'required',
-        'alamat' => 'required',
-        'instansi' => 'required',
-        'tujuan' => 'required',
-        'nama_pic' => 'required',
-        'jam_janji' => 'required',
-        'detail_tujuan' => 'required',
-        'jam_kedatangan' => 'required',
-        'suhu tubuh'=> 'required',
-        'survey1' => 'required',
-        'survey2' => 'required',
-        'survey3' => 'required',
-        'survey4' => 'required'
-    ]);
-
-    guest::create($request ->all());
-
-    return back()->with('success');
+    
+    public function index()
+    {
+    
+       $Guest = \Guest::get();
+        dd($Guest);
+        return view("guest.index",[
+            "Gues" => $data
+        ]);
     }
 }
