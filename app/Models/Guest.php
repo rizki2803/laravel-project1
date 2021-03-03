92cb800dcceb50a52095959ba7a9a222c5e805c0
Farhan Anshari Miraj <farhananshari77@gmail.com>
2021-03-03 15:12:15 +0700

    update model

---
+++ app/Models/Guest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Guest extends Model
+{
+    protected $table="guest_master";
+    protected $fillable=["id", "nama_tamu", "tlp", "alamat", "instansi", "tujuan", 
+    "nama_pic", "jam_janji", "detail_tujuan", "jam_kedatangan", "suhu_tubuh", 
+    "survey1", "survey2", "survey3", "survey4"];
+}
