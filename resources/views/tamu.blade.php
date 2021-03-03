d85224c2084084fb079d315be136f41778acb31a
Farhan Anshari Miraj <farhananshari77@gmail.com>
2021-03-03 13:33:57 +0700

    blade

---
+++ resources/views/tamu.blade.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+
+    <body>
+    <div style="background: #388BF2; font-size: 24px; color: white">
+    Simple library app
+</div>
+
+<div style="margin-top: 20px; margin-bottom: 20px">
+    <table border="1">
+        <a href="/books/add"> Add new book</a>
+
+        <thead>
+            <td> No </td>
+            <td> Title </td>
+            <td> Author </td>
+            <td> Publication </td>
+            <td> Year </td>
+            <td colspan="2"> Action </td>
+        </thead>
+        <tbody>
+            @foreach($books as $index => $book)
+                <tr>
+                    <td> {{ $index + 1 }}</td>
+                    <td> <a href="{{ url("/books", $book->id) }}" > {{ $book->title }} </a> </td>
+                    <td> {{ $book->author }} </td>
+                    <td> {{ $book->publication }} </td>
+                    <td> {{ $book->year }}</td>
+                    <td> <a href="{{ url("/books/edit", $book->id) }}"> Edit </a> </td>
+                    <td>
+                        <form method="POST" action="/books/{{$book->id}}">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+
+                            <div>
+                                <input type="submit" value="Delete">
+                            </div>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+<div style="background: #31B057; font-size: 20px; color: white">
+    Created with love and willingness to learn
+</div>
+    </body>
+</html>
