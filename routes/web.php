1e3b7bfa2cce1df13c4916a35e6e62cb1163373b
rizki2803 <rizkioi.rm@gmail.com>
2021-03-03 15:56:08 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tb_pegawai', 'PegawaiController@index');
-Route::get('/tb_pegawai', 'TamuController@index');
-Route::get('/tb_pegawai', 'TipeTamuController@index');
+Route::get('/guest_master', 'GuestController@index');
+
