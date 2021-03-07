5e6b555e935ca159c9cb5add3cc8ff95f63fef21
rizki2803 <rizkioi.rm@gmail.com>
2021-03-07 18:14:58 +0700

    push

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Guest extends Model
 {
+    use HasFactory;
     protected $table="guest_master";
     protected $fillable=["nama_tamu", "tlp", "alamat", "instansi", "tujuan", 
     "nama_pic", "jam_janji", "detail_tujuan", "jam_kedatangan", "suhu_tubuh", 
