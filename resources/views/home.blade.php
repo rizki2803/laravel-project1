e648eac72dfd2f17fbf3b863833ddbba1268b118
rizki2803 <rizkioi.rm@gmail.com>
2021-03-16 09:30:13 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                
+
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('Selamat Datang kituweh ') }}
+                    {{ __('Selamat Datang ') }}
                 </div>
             </div>
         </div>
